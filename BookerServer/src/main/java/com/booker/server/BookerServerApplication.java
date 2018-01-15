73ca2d91e579ff467bcddb8aac6ee3c611840f68
Cheechyo <kwj9211@gmail.com>
2018-01-15 18:45:18 +0900

    캐릭터인코딩

--- BookerServer/src/main/java/com/booker/server/BookerServerApplication.java
+++ BookerServer/src/main/java/com/booker/server/BookerServerApplication.java
@@ -2,11 +2,33 @@ package com.booker.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.Filter;
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 public class BookerServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookerServerApplication.class, args);
+	}
+
+	//결과를 출력시에 강제로 UTF-8설정하는 역활
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter() {
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+	}
+
+	//POST요청시 한글이 꺠지는 문제를 보완
+	@Bean
+	public Filter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
 	}
 }
