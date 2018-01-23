251bc6fdd27ad740d3a3ee1af0ba32baba77f2cd
Cheechyo <kwj9211@gmail.com>
2018-01-23 15:57:20 +0900

    메소드 구조 확립

--- BookerServer/src/main/resources/public/js/MyProfileController.js
+++ BookerServer/src/main/resources/public/js/MyProfileController.js
@@ -1,21 +1,49 @@
 function MyProfileController($scope, $http){
-	_.merge($scope, {
-		user : {
-			name : '-',
-			nameTag : '-',
-			numberOf : {
-				reservation : '-',
-				reading : '-',
-				rental : '-',
-				wish : '-'
-			}
-		},
+	console.log("MyProfileController loaded");
+
+	_.merge(($scope.user = {}), {
+		name : '-',
+		nameTag : '-',
+		numberOf : {
+			reservation : '-',
+			reading : '-',
+			rental : '-',
+			wish : '-'
+		}
 	});
 
 	$http.get('currentUserProfile')
 	.success(function(data){
 		_.merge($scope.user, data);
 		$scope.user.nameTag = data.name.toUpperCase().split('')[0]
-	})
-	console.log("MyProfileController loaded")
+	});
+
+	/* reservation */
+	_.merge(($scope.reservation = {}), {
+		model : {},
+		load : function(){
+
+		}
+	});
+	/* read */
+	_.merge(($scope.read = {}), {
+		model : {},
+		load : function(){
+
+		}
+	});
+	/* rental */
+	_.merge(($scope.rental = {}), {
+		model : {},
+		load : function(){
+
+		}
+	});
+	/* wish */
+	_.merge(($scope.wish = {}), {
+		model : {},
+		load : function(){
+
+		}
+	});
 }
